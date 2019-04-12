5073d7afe5809153ff54c0c2e927534b0f74b831
k-davis <kaseydavis216@gmail.com>
2019-04-12 14:17:31 -0700

    Added file start

---
+++ SnakeSnacks.java
@@ -0,0 +1,27 @@
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class SnakeSnacks extends JFrame{
+	private final int WINDOW_W = 500;
+	private final int WINDOW_H = 500;
+	
+	public SnakeSnacks(){
+		setTitle("SnakeSnacks");
+		setSize(WINDOW_W, WINDOW_H);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		
+		
+		
+		setVisible(true);
+	}
+	
+	
+	public static void main(String[] args){
+		new SnakeSnacks();
+	}
+}
