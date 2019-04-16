0cb50323cb1956334d4729342eee748fca035416
k-davis <kaseydavis216@gmail.com>
2019-04-15 19:47:57 -0700

    Fixed merged issues

--- Player.java
+++ Player.java
@@ -3,10 +3,10 @@ import java.awt.*;
 import java.util.Vector;
 import java.awt.event.*;
 
-public class Player {
+public class Player extends JPanel {
     private int roundScore = 0;
     private int gameScore = 0;
-	private JPanel panel;
+    private JPanel panel;
     private String color;
     private JTextField roundWins;
     private JTextField gameWins;
@@ -14,15 +14,18 @@ public class Player {
     private JLabel colorLabel;
     private JLabel roundLabel;
     private JLabel gameLabel;
-	private String playerOwner;
-	private Boolean justAteFood = false;
+    private String playerOwner;
+    private Boolean justAteFood = false;
 
-	private Vector<Snakebody> snake = new Vector<Snakebody>();
+    private Vector<Snakebody> snake = new Vector<Snakebody>();
 
-	public static enum Direction {UP, RIGHT, DOWN, LEFT};
-	private Direction curDir = Direction.UP;
+    public static enum Direction {
+        UP, RIGHT, DOWN, LEFT
+    };
 
-    public Player(String player){
+    private Direction curDir = Direction.UP;
+
+    public Player(String player) {
         playerOwner = player;
         panel = new JPanel();
         panel.setLayout(new GridLayout(3, 1));
@@ -38,66 +41,64 @@ public class Player {
 
         assignPanel(panel, playerOwner);
 
-	
     }
 
     public void incrementRoundWins(String playerOwner) {
-	    roundScore++;
-	}
-	
-	public void setDirection(Direction newOrient){
-		boolean isSameDir = curDir == newOrient;
-		boolean isOppositeDir = curDir.ordinal() == (newOrient.ordinal() + 2) % 4;
-		if(isSameDir || isOppositeDir) // don't want to go directly backwards
-			System.out.println("nah");
-		else {
-			curDir = newOrient;
-			System.out.println(curDir);
-		}
-	}
+        roundScore++;
+    }
 
-	public void doTimeStep(){
-		moveSnakeForward();
-	}
+    public void setDirection(Direction newOrient) {
+        boolean isSameDir = curDir == newOrient;
+        boolean isOppositeDir = curDir.ordinal() == (newOrient.ordinal() + 2) % 4;
+        if (isSameDir || isOppositeDir) // don't want to go directly backwards
+            System.out.println("nah");
+        else {
+            curDir = newOrient;
+            System.out.println(curDir);
+        }
+    }
 
-	private void moveSnakeForward(){
-		// Point nextHeadPos = getNextHeadPosition(snake, curDir);
+    public void doTimeStep() {
+        moveSnakeForward();
+    }
 
-		// A new body piece in the front
-		//  and remove the old one
-		// idx 0 is the head of the snake
-		//snake.insertElementAt(new Snakebody(nextHeadPos), 0);
+    private void moveSnakeForward() {
+        // Point nextHeadPos = getNextHeadPosition(snake, curDir);
 
-		// if the snake just ate food, you don't need to shrink
-		if(justAteFood){
-			justAteFood = false;
-		} else {
-			snake.remove(snake.size() - 1);
-		}
-	}
+        // A new body piece in the front
+        // and remove the old one
+        // idx 0 is the head of the snake
+        // snake.insertElementAt(new Snakebody(nextHeadPos), 0);
 
-	private static Point getNextHeadPosition(Vector<Snakebody> snake, Direction dir){
-		Point nextPosition = null; // = new Point(snake.get(0).getGridLocation());
-		switch(dir) {
-			case UP:
-				nextPosition.translate(0, -1);
-				break;
-			case RIGHT:
-				nextPosition.translate(1, 0);
-				break;
-			case DOWN:
-				nextPosition.translate(0, 1);
-				break;
-			case LEFT:
-				nextPosition.translate(-1, 0);
-				break;
-		}
-		return nextPosition;
-	}
-	
-	
-    public void assignPanel(JPanel panel, String playerOwner){
-      //  setBorder(BorderFactory.createTitledBorder(playerOwner));
+        // if the snake just ate food, you don't need to shrink
+        if (justAteFood) {
+            justAteFood = false;
+        } else {
+            snake.remove(snake.size() - 1);
+        }
+    }
+
+    private static Point getNextHeadPosition(Vector<Snakebody> snake, Direction dir) {
+        Point nextPosition = null; // = new Point(snake.get(0).getGridLocation());
+        switch (dir) {
+        case UP:
+            nextPosition.translate(0, -1);
+            break;
+        case RIGHT:
+            nextPosition.translate(1, 0);
+            break;
+        case DOWN:
+            nextPosition.translate(0, 1);
+            break;
+        case LEFT:
+            nextPosition.translate(-1, 0);
+            break;
+        }
+        return nextPosition;
+    }
+
+    public void assignPanel(JPanel panel, String playerOwner) {
+        // setBorder(BorderFactory.createTitledBorder(playerOwner));
 
         panel.add(colorLabel);
         panel.add(colorField);
@@ -106,7 +107,7 @@ public class Player {
         panel.add(gameLabel);
         panel.add(gameWins);
 
-        //add(panel);
+        add(panel);
     }
 
 }
